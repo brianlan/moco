f8f4259eacc07f9f2331d6cbdc0ccffd892c10d5
brianlan <brianlanbo@gmail.com>
2020-07-16 19:30:59 +0800

    added missing arguments for running main_moco.py and added step information printing as well.

--- train_moco_preemptible.sh
+++ train_moco_preemptible.sh
@@ -9,16 +9,28 @@ schedule=$6
 
 mkdir ${local_dataset_dir} -p
 mkdir ${local_model_checkpoint_dir} -p
-gsutil -m cp -r ${gs_dataset_dir}/* ${local_dataset_dir}
-gsutil -m cp ${gs_model_checkpoint_dir}/latest.pth.tar ${local_model_checkpoint_dir}
 
+echo "downloading data from ${gs_dataset_dir} to ${local_dataset_dir}"
+gsutil -m cp -r ${gs_dataset_dir}/* ${local_dataset_dir}
+echo "finished downloading data"
+
+echo "downloading latest model checkpoint from ${gs_model_checkpoint_dir} to ${local_model_checkpoint_dir}"
+gsutil -m cp ${gs_model_checkpoint_dir}/latest.pth.tar ${local_model_checkpoint_dir}
+echo "finished downloading model checkpoints"
+
+echo "start training.."
 python main_moco.py \
   -a resnet50 \
   --lr 0.03 \
   --batch-size 256 \
   --epochs ${epochs} \
   --schedule ${schedule} \
-  --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
+  --dist-url 'tcp://localhost:10001' 
+  --multiprocessing-distributed \ 
+  --world-size 1 --rank 0 \
   --mlp --moco-t 0.2 --aug-plus --cos \
-  --resume ${local_model_checkpoint_dir}/latest.pth.tar
+  --local-checkpoint-dir ${local_model_checkpoint_dir} \
+  --remote-checkpoint-dir ${gs_model_checkpoint_dir} \
+  --resume ${local_model_checkpoint_dir}/latest.pth.tar \
   ${local_dataset_dir}
+echo "finished training"
