2da1df7f9145eb2a45b35e3ecab4c142f2749cb2
brianlan <brianlanbo@gmail.com>
2020-07-18 12:03:51 +0800

    modified image-name used in instance-template

--- train_moco_preemptible.sh
+++ train_moco_preemptible.sh
@@ -28,7 +28,7 @@ gcloud beta compute instance-templates create $job_id \
     --machine-type n1-standard-16 \
     --boot-disk-size 500GB \
     --accelerator type=nvidia-tesla-v100,count=2 \
-    --image ubuntu1804-auto --image-project titanium-atlas-219621 \
+    --image ubuntu1804-cuda102 --image-project titanium-atlas-219621 \
     --maintenance-policy TERMINATE --restart-on-failure \
     --metadata ^___^job_id="$job_id"___local_dataset_dir="$local_dataset_dir"___local_model_checkpoint_dir="$local_model_checkpoint_dir"___gs_dataset_zip_path="$gs_dataset_zip_path"___gs_model_checkpoint_dir="$gs_model_checkpoint_dir"___lr="$lr"___batch_size="$batch_size"___epochs="$epochs"___schedule="$schedule" \
     --metadata-from-file startup-script=./train_moco_preemptible.sh \
